23c9cbd22340ae84631fa7119576933adfd8d58b
isaiah0311 <isaiahl0311@gmail.com>
2025-09-16 13:50:58 +0700

    Set up development pipeline

---
+++ src/main.c
@@ -0,0 +1,19 @@
+/**
+ * \file main.c
+ * \author Isaiah Lateer
+ *
+ * Entry point for the project.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Entry point for the project.
+ *
+ * \return Exit code
+ */
+int main() {
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
